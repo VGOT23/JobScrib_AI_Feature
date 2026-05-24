9b951b2d6371f7171e70a0fc7f286d2decd0ae72
VGOT23 <vansh1018sharma@gmail.com>
2026-05-24 23:57:41 +0530

    register API created

--- backend/src/controllers/application.controller.js
+++ backend/src/controllers/application.controller.js
@@ -1,24 +1,59 @@
-const jobApplicationmodel = require('../models/jobapplication.model')
-const mongoose = require('mongoose')
-async function getAllApplication(req,res) {
-    const user = req.user;
-    const allJobapplication = await jobApplicationmodel.find({user})
-    return res.status(200).json({
-        message : "all job application are feteched",
-        JobApplications : allJobapplication,
-        userId : user
-    })
+const jobApplicationmodel = require("../models/jobapplication.model");
+const mongoose = require("mongoose");
+
+async function getAllApplication(req, res) {
+  const user = req.user;
+  const allJobapplication = await jobApplicationmodel.find({ user });
+  return res.status(200).json({
+    message: "all job application are feteched",
+    JobApplications: allJobapplication,
+    userId: user,
+  });
 }
 
-async function registerApplication(req,res) {
-    
+async function registerApplication(req, res) {
+  try {
+    const user_id = req.user;
+    const {
+      company,
+      role,
+      status,
+      jobLink,
+      platform,
+      skillsRequired,
+      resumeId,
+      notes,
+    } = req.body;
+    const application = await jobApplicationmodel.create({
+      user_id: user_id,
+      company: company,
+      role: role,
+      status: status,
+      jobLink: jobLink,
+      platform: platform,
+      skillsRequired: skillsRequired,
+      resumeId: resumeId,
+      notes: notes,
+    });
+    return res.status(201).json({
+      message: "Job application created",
+      user_id: user_id,
+      job_application_id: application._id,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({
+      error: error,
+    });
+  }
 }
 
-async function deleteApplication(req,res) {
-    
-}
+async function deleteApplication(req, res) {}
 
-async function updateApplication(req,res) {
-    
-}
-module.exports = {getAllApplication ,registerApplication , deleteApplication , updateApplication};
+async function updateApplication(req, res) {}
+module.exports = {
+  getAllApplication,
+  registerApplication,
+  deleteApplication,
+  updateApplication,
+};
