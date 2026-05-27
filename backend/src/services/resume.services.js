c7d3fc3eb4ffa785476eb217d4c1d8f00def0a72
VGOT23 <vansh1018sharma@gmail.com>
2026-05-27 23:58:43 +0530

    upload file updated

--- backend/src/services/resume.services.js
+++ backend/src/services/resume.services.js
@@ -4,6 +4,18 @@ const client = new ImageKit({
   privateKey: process.env["IMAGEKIT_PRIVATE_KEY"],
 });
 
+async function uploadResume(file) {
+  try {
+    const response = await client.files.upload({
+      file,
+      fileName: 'resume' + Date.now(),
+      folder : 'JobSrib-resume'
+    });
+    console.log(response);
+  } catch (error) {
+    return;
+  }
+}
 
 module.exports = {uploadResume};
 
